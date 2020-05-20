615a2ac8f5b6a707b2705f4cfbc781cdc5047db5
luka <DoBok444>
2020-05-20 12:46:55 +0200

    Also added a test with strings

--- lib/model/sorting/insertion_sort.dart
+++ lib/model/sorting/insertion_sort.dart
@@ -1,5 +1,7 @@
 class InsertionSort {
   void sort(List toBeSorted) {
+    if (toBeSorted.length == 0) return;
+
     int length = toBeSorted.length;
     for (int i = 0; i < toBeSorted.length; i++) {
       for (int j = i; j > 0 && less(toBeSorted[j], toBeSorted[j-1]); j--) {
--- test/model/sorting/insertion_sort_test.dart
+++ test/model/sorting/insertion_sort_test.dart
@@ -4,7 +4,7 @@ import 'package:temp/model/sorting/insertion_sort.dart';
 void main() {
   var insertion_sort;
 
-  setupTest() {
+  setUp() {
     insertion_sort = new InsertionSort();
   }
 
@@ -14,21 +14,34 @@ void main() {
       for (var i = length; i > 0; i--) {
         list.add(i);
       }
-      
+
       return list;
     }
   }
 
   group('Test of insertion sort with integers', () {
-    test('Worst case for insertion sort, with 100 integers', () {
-      setupTest();
+    setUp();
 
+    test('Worst case for insertion sort, with 100 integers', () {
       var list = buildWorstCaseIntegers(100, 'int');
       insertion_sort.sort(list);
 
       for (var i = 0; i < list.length; i++) {
         expect(list[i], i + 1);
       }
+    });
+  });
+
+  group('Test of insertion sort with String', () {
+    setUp();
+
+    test('Simple test of abc sort in reverse order', () {
+      var list = ['c', 'b', 'a'];
+
+      insertion_sort.sort(list);
+      expect(list[0], 'a');
+      expect(list[1], 'b');
+      expect(list[2], 'c');
     });
   });
 }
