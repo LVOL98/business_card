3dff5ea5b225298000830511e2e284fb2e597429
luka <DoBok444>
2020-05-27 09:47:51 +0200

    Need to check another branch

---
+++ lib/model/tries/radix/radix_node.dart
@@ -0,0 +1,11 @@
+class RadixNode {
+  String _prefix;
+  int _value;
+  List _children;
+
+  RadixNode(this._prefix, this._value);
+
+  getPrefix() { return _prefix; }
+
+  getValue() { return _value; }
+}
---
+++ lib/model/tries/radix/radix_util.dart
@@ -0,0 +1,10 @@
+import 'dart:math';
+
+class RadixUtil {
+  largestPrefixLength(String a, String b) {
+    var len = 0;
+    for (var i = 0; i < min(a.length, b.length); ++i) {
+      if (a.)
+    }
+  }
+}
