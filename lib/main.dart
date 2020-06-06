9f46dc00634ae946604d8fa3a7999acb189b1fa7
luka <DoBok444>
2020-06-06 10:03:29 +0200

    More structure

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:temp/theme/style.dart';
 import 'package:temp/view/login.dart';
 
-void main() {
-  runApp(MaterialApp(
-    theme: AppStyle.lighTheme,
-    title: 'TODO',
-    home: LoginPage(), // TODO: problably shouldn't be home
-  ));
+class Main extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: AppStyle.lighTheme,
+      title: 'TODO',
+      home: LoginPage(), // TODO: problably shouldn't be home
+    );
+  }
 }
+
+void main() => runApp(Main());
 
 // Colors
 // https://coolors.co/3a405a-4d536b-685044-776155-4b9980-81b29a-a3d1bd-e07a5f-e68f73
---
+++ lib/view/about/about_master.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class AboutPageMaster extends StatefulWidget {
+  @override
+  _AboutPageMasterState createState() => _AboutPageMasterState();
+}
+
+class _AboutPageMasterState extends State<AboutPageMaster> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
