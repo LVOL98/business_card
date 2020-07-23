5786a2841bd83234ba302964723361a0bfa91737
luka <DoBok444>
2020-07-23 14:24:42 +0200

    Fixed web top navigatio
    Now is a stateless widget, and has a placeholder

--- lib/components/nav/web_top_navigation.dart
+++ lib/components/nav/web_top_navigation.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
 
-/// Creates the top navigation on web, in which the content is the 
+/// Creates the top navigation on web, in which the content is the
 /// widgets displayed from left to right horizontally on the bar
-Widget webTopNavigation(BuildContext context, List<Widget> content) {
-  return SliverAppBar(
-    expandedHeight: MediaQuery.of(context).size.height * 0.05,
-    flexibleSpace: FlexibleSpaceBar(
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          for (Widget item in content)
-            Padding(padding: EdgeInsets.fromLTRB(0, 12, 0, 0), child: item), // TODO: find a better solution
-        ],
+class WebTopNavigation extends StatelessWidget {
+  List<Widget> content;
+
+  WebTopNavigation(this.content);
+
+  WebTopNavigation.placeholder() {
+    this.content = [
+      Text('Some placeholder text'),
+      Text('Some more placeholder text')
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      expandedHeight: MediaQuery.of(context).size.height * 0.05,
+      flexibleSpace: FlexibleSpaceBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            for (Widget item in content)
+              Padding(
+                  padding: EdgeInsets.fromLTRB(0, 12, 0, 0),
+                  child: item), // TODO: find a better solution
+          ],
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
