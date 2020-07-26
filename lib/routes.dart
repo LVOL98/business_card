5bb8a46ff13da0b63898034de61459c8899ebbff
luka <DoBok444>
2020-07-26 22:55:40 +0200

    Added algorithms page to route

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/cupertino.dart';
-import 'package:temp/components/dialogs/not_implemented.dart';
+import 'package:temp/view/algorithms.dart';
 import 'package:temp/view/front/front_master.dart';
 import 'package:temp/view/home/home_master.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
   '/': (BuildContext context) => FrontPageMaster(),
   '/home': (BuildContext context) => HomePageMaster(),
+  '/home/algorithms': (BuildContext context) => AlgorithmPage(),
 };
