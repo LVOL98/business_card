4dcb695853172f7b548d3ed6d5d9e150d7f21c5d
luka <DoBok444>
2020-07-26 23:45:34 +0200

    Working component
    Need to find smart way to make bars equal so a given size fx 25% of screen size

--- lib/components/sorting_visualization/bar_sorting.dart
+++ lib/components/sorting_visualization/bar_sorting.dart
@@ -2,39 +2,82 @@ import 'package:flutter/material.dart';
 import 'package:temp/model/sorting/insertion_sort.dart';
 import 'package:temp/model/util/random_generator.dart';
 
-class BarSorting extends StatelessWidget {
-  List _insertionSortList = RandomGenerator().generatRandomIntegers(100, 100);
+class BarSorting extends StatefulWidget {
+  var sortingMethod;
+
+  BarSorting(this.sortingMethod);
+
+  @override
+  _BarSortingState createState() => _BarSortingState(sortingMethod);
+}
+
+class _BarSortingState extends State<BarSorting> {
+  var sortingMethod;
+
+  _BarSortingState(this.sortingMethod);
+
+  static const int _bar_size = 100;
+
+  List _insertionSortList = RandomGenerator().generatRandomIntegers(100, _bar_size);
+  InsertionSort _insertionSort = InsertionSort();
+
+  _sortAnimation() {
+    _insertionSort.sortAnimation(_insertionSortList, setState);
+  }
+
+  _clearSort() {
+    _insertionSortList = RandomGenerator().generatRandomIntegers(100, 100);
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     double countOfBars = 0;
 
-    return Container(
-      child: Row(
-        children: _insertionSortList.map(
-          (number) {
-            countOfBars++;
+    return Column(
+      children: [
+        Row(
+          children: _insertionSortList.map(
+            (number) {
+              countOfBars++;
 
-            return CustomPaint(
-              painter: BarPainter(2, countOfBars, number),
-            );
+              return CustomPaint(
+                painter: BarPainter(context, 2, countOfBars, number),
+              );
+            },
+          ).toList(),
+        ),
+        FlatButton(
+          child: Text('Sort'),
+          onPressed: () {
+            _sortAnimation();
           },
-        ).toList(),
-      ),
+        ),
+        FlatButton(
+          child: Text('Clear'),
+          onPressed: () {
+            _clearSort();
+          },
+        )
+      ],
     );
   }
 }
 
 class BarPainter extends CustomPainter {
-  double width, row;
+  BuildContext context;
+  double width;
+  double row;
   int length;
 
-  BarPainter(this.width, this.row, this.length);
+  BarPainter(this.context, this.width, this.row, this.length);
 
   @override
   void paint(Canvas canvas, Size size) {
-    canvas.drawLine(Offset(width * row, 0),
-        Offset(width * row, length.ceilToDouble()), Paint());
+    canvas.drawLine(
+        Offset((MediaQuery.of(context).size.width * 0.225) + (width * row), 0),
+        Offset((MediaQuery.of(context).size.width * 0.225) + (width * row), length.ceilToDouble()),
+        Paint());
   }
 
   @override
--- lib/view/algorithms/algorithms_mobile.dart
+++ lib/view/algorithms/algorithms_mobile.dart
@@ -10,7 +10,7 @@ class _AlgorithmPageMobileState extends State<AlgorithmPageMobile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BarSorting(),
+      body: BarSorting(null),
     );
   }
 }
